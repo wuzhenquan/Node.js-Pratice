3f534899dea67aed1e3c929d3139375c1641b46d
巫镇泉 <wuzhenquan1990@gmail.com>
2018-06-07 00:26:25 +0800

    update chat.io

--- chat.io/public/chat.js
+++ chat.io/public/chat.js
@@ -1,16 +1,35 @@
 window.onload = () => {
     const socket = io.connect()
     socket.on('connect', () => {
-        // 通过 join 事件发送昵称
-        socket.emit('join', prompt('what is your nickname?'))
-        // 显示聊天窗口
-        document.querySelector('#chat').style.display = 'block'
-
-        socket.on('announcement',(msg)=>{
+        socket.emit('join', prompt('what is your nickname?')) // 通过 join 事件发送昵称
+        document.querySelector('#chat').style.display = 'block' // 显示聊天窗口
+        socket.on('announcement', (msg) => {
             const li = document.createElement('li')
             li.className = 'announctment'
             li.innerHTML = msg
-            document.querySelector('#message').appendChild(li)
+            document.querySelector('#messages').appendChild(li)
         })
     })
+
+    // 将接收到的信息加载在页面上
+    function addMessage(from, text) {
+        var li = document.createElement('li')
+        li.className = 'message'
+        li.innerHTML = `<b>${from}</b>: ${text}`
+        document.querySelector('#messages').appendChild(li)
+    }
+
+    const input = document.querySelector('#input')
+    document.querySelector('#button').onclick = function () {
+        addMessage('me', input.value)
+        socket.emit('text', input.value) // 发送消息
+
+        // 重置输入框
+        input.value = ''
+        input.focus()
+
+        return false
+    }
+
+    socket.on('text',addMessage) // 接受服务端发送过来的消息
 }
--- chat.io/server.js
+++ chat.io/server.js
@@ -2,16 +2,23 @@ const express = require('express')
 const app = express();
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-server.listen(3000)
 const bodyParser = require('body-parser')
+
+server.listen(3000)
 
 app.use(bodyParser.urlencoded({ extended: true })) // 解析查询字符串为对象的格式
 app.use(express.static('public'))
+
 io.sockets.on('connection', (socket) => {
-    console.log('Someone connected')
     // 监听 join 事件
     socket.on('join', (name) => {
         socket.nickname = name;
+        // 注意 socket.broadcast.emit 和 socket.emit 的区别
         socket.broadcast.emit('announcement', name + 'joined the chat.')
+    })
+
+    // 接受客户端发来的消息
+    socket.on('text', function (msg) {
+        socket.broadcast.emit('text', socket.nickname, msg) // 发送消息到客户端
     })
 })
