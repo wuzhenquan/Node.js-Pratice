debebfcb0e46b3544f6aa067330abc3c65d808bc
巫镇泉 <wuzhenquan1990@gmail.com>
2018-06-14 23:05:46 +0800

    去分号

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,20 +1,20 @@
 module.exports = function (app) {
   app.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
-  });
+    res.render('index', { title: 'Express' })
+  })
   app.get('/reg', (req, res) => {
-    res.render('reg', { title: 'register' });
-  });
+    res.render('reg', { title: 'register' })
+  })
   app.get('/login', (req, res) => {
-    res.render('login', { title: 'login' });
-  });
+    res.render('login', { title: 'login' })
+  })
   app.post('/login', (req, res) => {
-  });
+  })
   app.get('/post', (req, res) => {
-    res.render('post', { title: 'posts' });
-  });
+    res.render('post', { title: 'posts' })
+  })
   app.post('/post', (req, res) => {
-  });
+  })
   app.get('/logout', (req, res) => {
-  });
-};
+  })
+}
