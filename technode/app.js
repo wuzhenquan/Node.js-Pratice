c9c37691b746f2d06ac0de128a24904c66721c46
巫镇泉 <wuzhenquan1990@gmail.com>
2018-07-14 17:49:56 +0800

    **technode** 添加登录功能

--- technode/app.js
+++ technode/app.js
@@ -1,14 +1,64 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+var express = require('express')
+var app = express()
+var path = require('path')
+var port = process.env.PORT || 3000
+var bodyParser = require('body-parser')
+var cookieParse = require('cookie-parser')
+var session = require('express-session')
+var Controllers = require('./controllers')
+var logger = require('morgan')
 
-const port = process.env.PORT || 3000
 
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(cookieParse())
+app.use(session({
+    secret: 'technode',
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }, // 30 days
+}))
 // 与通常的 Express 项目一样， 我们将静态文件放在 static 目录下。
 // 在 static 目录下还会放 index.html 文件作为整个应用的启动页面。
 // 除了静态文件的请求外，其他所有的 HTTP 请求都会输出 index.html 文件，
 // 服务端不关心路由，所有的路由都交给在浏览器端的 JS 去处理
 app.use(express.static(path.join(__dirname, '/static')))
+app.get('/api/validate', function (req, res) {
+    var _userId = req.session.userId;
+    if (_userId) {
+        Controllers.User.findUserById(_userId, function (err, user) {
+            if (err) {
+                res.status(401).json({ msg: err }) // 新的 api 是这样的，res.status(500).json({ error: 'message' }); https://expressjs.com/en/api.html 看看要不要改
+            } else {
+                res.json(user)
+            }
+        })
+    } else {
+        res.status(401).json(null)
+    }
+})
+app.post('/api/login', function (req, res) {
+    var email = req.body.email
+    if (email) {
+        Controllers.User.findByEmailOrCreate(email, function (err, user) {
+            if (err) {
+                res.status(500).json({ msg: err })
+            } else {
+                req.session._userId = user._id
+                res.json(user)
+            }
+        })
+    } else {
+        res.status(403)
+    }
+})
+app.get('/api/logout', function (req, res) {
+    req.session._userId = null
+    res.status(401).json(null)
+})
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, './static/index.html'))
 })
@@ -19,12 +69,12 @@ const server = app.listen(port, () => {
 // 在服务端增加 Socket 服务
 var io = require('socket.io').listen(server)
 var messages = []
-io.sockets.on('connection', function(socket) {
-  socket.on('messages.read', function() {
-    socket.emit('messages.read', messages)
-  })
-  socket.on('messages.create', function(message) {
-    messages.push(message)
-    io.sockets.emit('messages.add', message)
-  })
+io.sockets.on('connection', function (socket) {
+    socket.on('messages.read', function () {
+        socket.emit('messages.read', messages)
+    })
+    socket.on('messages.create', function (message) {
+        messages.push(message)
+        io.sockets.emit('messages.add', message)
+    })
 })
--- technode/static/technode.js
+++ technode/static/technode.js
@@ -1,94 +1,28 @@
 
-angular.module('techNodeApp', [])
-
-angular.module('techNodeApp').factory('socket', function ($rootScope) {
-    // var socket = io.connect('/')
-    //为了避免跨域请求，需要将原来的var socket = io.connect('/')改成下面这一行
-    // var socket = io.connect('http://localhost:3000/')
-    //或者采用socket官方主页上面的方法 var socket = io()
-    var socket = io();
-    return {
-        on: function (eventName, callback) {
-            socket.on(eventName, function () {
-                var args = arguments
-                $rootScope.$apply(function () {
-                    callback.apply(socket, args)
-                })
-            })
-        },
-        emit: function (eventName, data, callback) {
-            socket.emit(eventName, data, function () {
-                var args = arguments
-                $rootScope.$apply(function () {
-                    if (callback) {
-                        callback.apply(socket, args)
-                    }
-                })
-            })
-        }
-    }
-})
-
-// textarea 改成通过 ctrl+enter 来换行, 而 enter 触发绑定的方法（ createMessage 方法）
-angular.module('techNodeApp').directive('ctrlEnterBreakLine', function () {
-    return function (scope, element, attrs) {
-        var ctrlDown = false
-        element.bind("keydown", function (evt) {
-            if (evt.which === 17) {
-                ctrlDown = true
-                setTimeout(function () {
-                    ctrlDown = false
-                }, 1000)
-            }
-            if (evt.which === 13) {
-                if (ctrlDown) {
-                    element.val(element.val() + '\n')
-                } else {
-                    scope.$apply(function () {
-                        scope.$eval(attrs.ctrlEnterBreakLine);
-                    });
-                    evt.preventDefault()
-                }
-            }
-        });
-    };
-});
-
-// 当用户按下回车时，将消息通过 socket 发送给服务端
-angular.module('techNodeApp').controller('MessageCreatorCtrl', function ($scope, socket) {
-    // $scope.newMessage 是通过 ng-model 与 textarea 直接绑定的
-    // createMessage 是通过 ctrl-enter-break-line 与 textarea 直接绑定的
-    $scope.createMessage = function () {
-        socket.emit('messages.create', $scope.newMessage)
-        $scope.newMessage = ''
-    }
-})
-
-// 当消息出现滚动条时，该组件使得滚动条能随着消息的增加自动滚动到底部
-angular.module('techNodeApp').directive('autoScrollToBottom', function () {
-    return {
-        link: function (scope, element, attrs) {
-            scope.$watch(
-                function () {
-                    return element.children().length;
-                },
-                function () {
-                    element.animate({
-                        scrollTop: element.prop('scrollHeight')
-                    }, 1000);
-                }
-            );
-        }
-    };
-});
-
-angular.module('techNodeApp').controller('RoomCtrl', function ($scope, socket) {
-    $scope.messages = []
-    socket.on('messages.read', function (messages) {
-        $scope.messages = messages
+angular.module('techNodeApp', ['ngRoute']).run(function ($window, $rootScope, $http, $location) {
+    // 应用启动时验证 ‘/api/validate' 接口
+    // 如果用户已登录 请求 ‘/’ url
+    // 如果用户未登录 请求 ‘/login' url
+    $http({
+        url: '/api/validate',
+        method: 'GET'
+    }).success(function (user) { // 
+        $rootScope.me = user
+        $location.path('/')
+    }).error(function (data) { // 
+        $location.path('/login')
     })
-    socket.on('messages.add', function (message) {
-        $scope.messages.push(message)
+
+    $rootScope.logout = function(){
+        $http({
+            url: '/ajax/logout',
+            method: 'GET'
+        }).success(function (user) {
+            $rootScope.me = null
+            $location.path('/login')
+        })
+    }
+    $rootScope.$on('login', function(evt, me){
+        $rootScope.me = me
     })
-    socket.emit('messages.read')
 })
