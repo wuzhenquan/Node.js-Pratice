305ba638df1f8f5bc46793d69be70e1da456777b
巫镇泉 <wuzhenquan1990@gmail.com>
2018-07-16 23:11:27 +0800

    **technode** 聊天记录显示用户名

--- technode/static/controllers/message-creator.js
+++ technode/static/controllers/message-creator.js
@@ -1,4 +1,4 @@
-// 当用户按下回车时，将消息通过 socket 发送给服务端
+// 将消息通过 socket 发送给服务端
 angular.module('techNodeApp').controller('MessageCreatorCtrl', function ($scope, socket) {
     // $scope.newMessage 是通过 ng-model 与 textarea 直接绑定的
     // createMessage 是通过 ctrl-enter-break-line 与 textarea 直接绑定的
